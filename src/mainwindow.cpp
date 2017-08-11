6f59cb2d866561fa2df181d80b03b19a63842a2a
Michal Staromiejski <michal.staromiejski@gmail.com>
2017-08-11 20:07:37 +0200

    Qt GUI: added controls, view area and reaction on Draw button to resize image widget.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,9 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QValidator * doubleValidator = new QDoubleValidator(this);
+    ui->reminLineEdit->setValidator(doubleValidator);
+    ui->remaxLineEdit->setValidator(doubleValidator);
+    ui->imminLineEdit->setValidator(doubleValidator);
+    ui->immaxLineEdit->setValidator(doubleValidator);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_drawButton_clicked()
+{
+    int width = ui->imageWidthSpinBox->value();
+    int height = ui->imageHeightSpinBox->value();
+    ui->imageWidget->setFixedSize(width, height);
 }
